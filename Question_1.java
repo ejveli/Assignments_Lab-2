8912a47d901866ed11561a84ac8beca1da4a38bb
ejveli <71405672+ejveli@users.noreply.github.com>
2020-10-24 08:11:35 +0100

    Update Question_1.java

--- Question_1.java
+++ Question_1.java
@@ -1,54 +1,54 @@
-public static void main(String[] args) {
+public class Question_1 {
+
+	/*
+	 * Write a program with a loop that lets the user enter a series of integer
+	 * numbers. After all the numbers have been entered, the program should display
+	 * the largest and smallest numbers entered.
+	 */
+
+	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int smallestNumber;
 		int largestNumber;
-		int enteredNumber;
+		int firstnumber;
+		int nextNumber;
 		int check = 1; // I assume check=1 in order to enter while loop
 
 		System.out.println("Enter your first number: ");
-		int number1 = scan.nextInt();
+		firstnumber = scan.nextInt();
 
 		System.out.println("Enter your next number: ");
-		int number2 = scan.nextInt();
+		nextNumber = scan.nextInt();
 
-		if (number1 > number2) {
-
-			smallestNumber = number2;
-			largestNumber = number1;
+		if (firstnumber > nextNumber) {
+			smallestNumber = nextNumber;
+			largestNumber = firstnumber;
 		} else {
-			smallestNumber = number1;
-			largestNumber = number2;
+			smallestNumber = firstnumber;
+			largestNumber = nextNumber;
 		}
-
 		while (check == 1) {
 
 			System.out.println("Do you want to enter another number: 0-No, 1-Yes");
 			check = scan.nextInt();
 
 			if (check == 0) {
-
 				break;
-
 			}
 
 			if (check == 1) {
-
 				System.out.println("Enter your next number: ");
-				enteredNumber = scan.nextInt();
+				nextNumber = scan.nextInt();
 
-				if (enteredNumber > largestNumber) {
-
-					largestNumber = enteredNumber;
-
-				} else if (enteredNumber < smallestNumber) {
-
-					smallestNumber = enteredNumber;
+				if (nextNumber > largestNumber) {
+					largestNumber = nextNumber;
+				} else if (nextNumber < smallestNumber) {
+					smallestNumber = nextNumber;
 				}
-
 			}
 			check = 1; // to return beginning of while loop if another number entered instead of 1 or 0
 		}
 		System.out.println("Smallest number is: " + smallestNumber);
 		System.out.println("Largest number is: " + largestNumber);
-
 	}
+}
